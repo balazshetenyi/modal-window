741fe0727c4ce6bea381bd10b76441c1e3feb035
balazshetenyi <balazshetenyi82@gmail.com>
2020-07-21 16:34:00 +0100

    simple modal window

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('btn').addEventListener('click', function () {
+    document.getElementById('overlay').style.display = 'inline-block'
+})
+document.getElementById('close').addEventListener('click', function () {
+    document.getElementById('overlay').style.display = 'none'
+})
